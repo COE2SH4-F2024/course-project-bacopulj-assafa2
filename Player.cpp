4682e54fa4b8f1f4d34845790e263f3a451af793
John <bacopulj@mcmaster.ca>
2024-11-30 23:16:06 -0500

    Initial Commit

--- Player.cpp
+++ Player.cpp
@@ -7,27 +7,100 @@ Player::Player(GameMechs* thisGMRef)
     myDir = STOP;
 
     // more actions to be included
+    playerPos.setObjPos(10, 5, '*');
 }
 
 
 Player::~Player()
 {
     // delete any heap members here
+    delete mainGameMechsRef;    
 }
 
 objPos Player::getPlayerPos() const
 {
     // return the reference to the playerPos arrray list
+    return playerPos;
 }
 
 void Player::updatePlayerDir()
 {
-        // PPA3 input processing logic          
+    // PPA3 input processing logic
+    char input = mainGameMechsRef->getInput();
+    if (input != 0) // if not null character
+    {
+        switch (input)
+        {
+            case 27: // exit
+                mainGameMechsRef->setExitTrue();
+                break;
+            case 87:
+            case 119:
+                if (myDir == LEFT || myDir == RIGHT || myDir == STOP)
+                {
+                    myDir = UP;
+                }
+                break;
+            case 83:
+            case 115:
+                if (myDir == LEFT || myDir == RIGHT || myDir == STOP)
+                {
+                    myDir = DOWN;
+                }
+                break;
+            case 65:
+            case 97:
+                if (myDir == UP || myDir == DOWN || myDir == STOP)
+                {
+                    myDir = LEFT;
+                }
+                break;
+            case 68:
+            case 100:
+                if (myDir == UP || myDir == DOWN || myDir == STOP)
+                {
+                    myDir = RIGHT;
+                }
+                break;
+            default:
+                break;
+        }
+        input = 0;
+    }      
 }
 
 void Player::movePlayer()
 {
     // PPA3 Finite State Machine logic
+    switch (myDir)
+    {
+    case UP:
+        if (playerPos.pos->y == 1)
+            playerPos.pos->y = mainGameMechsRef->getBoardSizeY() - 2;
+        else
+            playerPos.pos->y--;
+        break;
+    case DOWN:
+        if (playerPos.pos->y == mainGameMechsRef->getBoardSizeY() - 2)
+            playerPos.pos->y = 1;
+        else
+            playerPos.pos->y++;
+        break;
+    case LEFT:
+        if (playerPos.pos->x == 1)
+            playerPos.pos->x = mainGameMechsRef->getBoardSizeX() - 2;
+        else
+            playerPos.pos->x--;
+        break;
+    case RIGHT:
+        if (playerPos.pos->x == mainGameMechsRef->getBoardSizeX() - 2)
+            playerPos.pos->x = 1;
+        else
+            playerPos.pos->x++;
+        break;
+    default:
+        break;
+    }
 }
 
 // More methods to be added
--- Project.cpp
+++ Project.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include "MacUILib.h"
 #include "objPos.h"
+#include "GameMechs.h"
+#include "Player.h"
 
 using namespace std;
 
 #define DELAY_CONST 100000
+#define WIDTH 30
+#define HEIGHT 15
 
-bool exitFlag;
+GameMechs* mainGameMechs;
+Player* player;
 
 void Initialize(void);
 void GetInput(void);
@@ -15,14 +20,12 @@ void DrawScreen(void);
 void LoopDelay(void);
 void CleanUp(void);
 
-
-
 int main(void)
 {
 
     Initialize();
 
-    while(exitFlag == false)  
+    while(mainGameMechs->getExitFlagStatus() == false)  
     {
         GetInput();
         RunLogic();
@@ -40,22 +43,43 @@ void Initialize(void)
     MacUILib_init();
     MacUILib_clearScreen();
 
-    exitFlag = false;
+    mainGameMechs = new GameMechs(WIDTH, HEIGHT);
+    player = new Player(mainGameMechs);
 }
 
 void GetInput(void)
 {
-   
+   int hasChar = MacUILib_hasChar();
+
+   if (hasChar) {
+        char input = MacUILib_getChar();
+        mainGameMechs->setInput(input);
+   }
 }
 
 void RunLogic(void)
 {
-    
+    player->updatePlayerDir();
+    player->movePlayer();
 }
 
 void DrawScreen(void)
 {
     MacUILib_clearScreen();    
+    for (int i = 0; i < mainGameMechs->getBoardSizeY(); i++) {
+        for (int j = 0; j < mainGameMechs->getBoardSizeX(); j++) {
+            if (i == player->getPlayerPos().pos->y && j == player->getPlayerPos().pos->x) {
+                MacUILib_printf("%c", player->getPlayerPos().symbol);
+            } else if (j == mainGameMechs->getBoardSizeX() - 1) {
+                MacUILib_printf("#\n");
+            }
+            else if (i == 0 || i == mainGameMechs->getBoardSizeY() - 1 || j == 0) {
+                MacUILib_printf("#");
+            } else {
+                MacUILib_printf(" ");
+            }
+        }
+    }
 }
 
 void LoopDelay(void)
